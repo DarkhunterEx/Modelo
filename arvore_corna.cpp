192e94f4deac856b0e9c102b5ba5b3e35d5af2dd
DarkhunterEx <106594935+DarkhunterEx@users.noreply.github.com>
2022-11-21 07:43:41 -0300

    Update arvore_corna.cpp

--- arvore_corna.cpp
+++ arvore_corna.cpp
@@ -240,7 +240,7 @@ class AVLTree{
                         (*r)->fb=0;
                         return false;
                     }
-                    if(((*r)->right)->fb==-1){
+                    if(((*r)->left)->fb==1){
                         AVLTree::RSD(r);
                         return false;
                     }
@@ -259,7 +259,7 @@ class AVLTree{
                         (*r)->fb=0;
                         return false;
                     }
-                    if(((*r)->left)->fb==1){
+                    if(((*r)->right)->fb==-1){
                         AVLTree::RSE(r);
                         return false;
                     }
@@ -312,7 +312,7 @@ class AVLTree{
                         (*r)->fb=-1;
                         return false;
                     }
-                    if(((*r)->left)->fb<=0){
+                    if(((*r)->left)->fb>=0){
                         AVLTree::RSD(r);
                         return true;
                     }
@@ -342,7 +342,7 @@ class AVLTree{
                         (*r)->fb=-1;
                         return false;
                     }
-                    if(((*r)->left)->fb<=0){
+                    if(((*r)->left)->fb>=0){
                         AVLTree::RSD(r);
                         return true;
                     }
